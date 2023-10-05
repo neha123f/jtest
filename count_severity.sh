cd75c3a75fd14180e54fe18dfa43c25be6a18184
neha123f <121794908+neha123f@users.noreply.github.com>
2023-10-05 11:51:36 +0530

    Update count_severity.sh

--- count_severity.sh
+++ count_severity.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Specify the JSON filename (assuming it exists in the current directory)
-json_file="input.json"
+json_file="sechub_report_atcpps_dcf6ffa9-f4ca-4954-b568-88d5a8c5e58c.json"
 
 # Use jq to count and format severity levels
 severity_count=$(jq '[.. | select(.severity?)] | group_by(.severity) | map({severity: .[0].severity, count: length})' "$json_file")
